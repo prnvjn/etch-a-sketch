0f670bea80216017ddff58d711c63300c67f3d4d
Pranav <prnvjn@gmail.com>
2021-07-06 13:28:52 +0530

    added a slider

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
 const container = document.querySelector("#container")
 const resetButton = document.querySelector('#reset')
-
+let slider = document.querySelector("#myRange")
 //for creating the grid
-for (let i = 0 ; i < 400;i++){
-    const div = document.createElement('div')
-    div.classList = "element"
-    
-   container[i]=container.appendChild(div)
+
+
+function gridSystem(value){
+    grid = value*value
+    for (let i = 0 ; i < grid ;i++){
+        const div = document.createElement('div')
+        div.classList = "element"
+        div.style.width= `${480/value}px`
+        div.style.height= `${480/value}px`
+       container[i]=container.appendChild(div)
+    }
 }
+
 //for hover 
-const elements = document.querySelectorAll(".element")
-elements.forEach((element)=>
-element.addEventListener('mousemove', (e)=>{
-    e.target.classList.add('highLight')
-} )
-)
+function hover(){
+    const elements = document.querySelectorAll(".element")
+    elements.forEach((element)=>
+    element.addEventListener('mousemove', (e)=>{
+        e.target.classList.add('highLight')
+    } )
+    )
+}
+
 // reset the code
 let reset = ()=> {
+    const elements = document.querySelectorAll(".element")
     elements.forEach((element)=>resetButton.addEventListener('click',()=>
     {
         if(element.classList.contains('highLight')){
             element.classList.remove('highLight')
         }
-}))
+    }))
+      
 }
 
+
+
+gridSystem(20)
+
+hover()
 reset()
+newGrid()
+function newGrid() {
+    slider.addEventListener("input", () => {
+        let grid = slider.value;
+    });
+    return grid
+}
 
